8679805bb9e021fd524cabc7598c9f3a9cd0061c
Jahanzaib0070 <mohammadjahanzaib007@gmail.com>
2024-07-11 22:54:28 +0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = ()=>{
     return <>
         <div className="header">
             <img src="https://www.gstatic.com/images/branding/product/2x/keep_2020q4_48dp.png" width="70px" alt="Logo" />
-            <h1>Google Keep</h1>
+            <h1 className="headerh1">Google Keep</h1>
         </div>
     </>
 }
